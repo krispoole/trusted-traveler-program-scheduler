ed3ecc7bc92d4c0e535d56426e4e218c323db4b9
kris <krispoole@pm.me>
2024-08-12 16:13:48 -0500

    updated

--- scheduler/src/main/java/com/krispoole/scheduler/service/EmailService.java
+++ scheduler/src/main/java/com/krispoole/scheduler/service/EmailService.java
@@ -2,7 +2,6 @@ package com.krispoole.scheduler.service;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -10,7 +9,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmailService {
 
-    @Autowired
     private JavaMailSender mailSender;
 
     public void sendSimpleMessage(String to, String subject, String text) {
